596b3679945ce9f521faccbe5ff03fd92bf690fb
Yuanzhe Li <zzzhe1990@gmail.com>
2018-04-30 19:30:42 -0400

    GPU_TILE is now running

--- LCS/parallel.cu
+++ LCS/parallel.cu
@@ -8,70 +8,71 @@ typedef unsigned long long int UINT;
 
 using namespace std;
 
-__global__ void GPU(int *dev_table, int *dev_arr1, int *dev_arr2, int startIdx, int curjobs, const int rowsize, int startx, int starty){
-	int thread = blockIdx.x * blockDim.x + threadIdx.x;
-	
-	if (thread < curjobs){
-		int idx = startIdx + (thread * rowsize - thread);
-		int x = startx - thread;
-		int y = starty + thread;
-		
-		*(dev_table + idx) = max(*(dev_table+idx-1), *(dev_table+idx-rowsize));
-		
-		if (dev_arr1[x] == dev_arr2[y]){
-//			printf("if equal, before, idx: %d, dev_table[idx]: %d; pre: %d, dev_table[idx-rowsize-1]: %d \n", idx, dev_table[idx], idx-rowsize-1, dev_table[idx-rowsize-1]);
-			*(dev_table+idx) = *(dev_table+ (idx - rowsize - 1) ) + 1;
-//			printf("after, dev_table[idx]: %d\n", dev_table[idx]);
-		}		
-//		printf("thread: %d, idx: %d, x: %d, y: %d, arr1[x]: %d, arr2[y]: %d \n", thread, idx, x, y, dev_arr1[x], dev_arr2[y]);
-	}
-//	__threadfence();
-}
 
-
-
-__global__ void GPU(const int tilesize, const int poolsize, const int threadNum, const int maxThreads, int *dev_table, const int rowsize, const int maxlevel, int tileX, int lenY){
+__global__ void GPU(const int tilesize, const int poolsize, const int maxThreads, 
+			int *dev_table, const int rowsize, const int maxlevel, int tileX, int lenY, int *dev_arr1, int *dev_arr2){
 	//This code has to ensure n2 size is the multiple of 128. And n2 is no smaller than n1, where n2 is row array size, n1 is colum array size
 	//on K40, tile size is max to 48K, which is 128*96; on pascal and volta, tile size is max to 64K which is 128*128
 	//For running on K40, we reserve the shared memory space for a 64*64 tile. Because of the dependency, the actual shared memory size is 96 * 96. 96 is picked for ensuring memory coalscing.
-	__shared__ int tile[9216];
+	__shared__ int table[9216];
 
-	int thread = blockIdx.x * blockDim.x + threadIdx.x;
+	int thread = threadIdx.x;
 	int idx = thread;
 	int address = thread;
 	int curjobs = 0;
 	int curlevel = 1;
 	int startIdx, startx, starty;
-
+	int tableX = tileX + poolsize;
+	int x,y;
+//	printf("thread: %d, idx: %d, address: %d, tableX: %d\n", thread, idx, address, tableX);
 	while(idx < tilesize){
-		tile[idx] = dev_table[address];
+//		printf("thread: %d, idx: %d, address: %d\n", thread, idx, address);
+		table[idx] = dev_table[address];
 		address += rowsize;
-		idx += threadNum;
+		idx += blockDim.x;
 	}
 	
 	while (curlevel <= maxlevel){
-		if (curjobs < lenY)
+		if (curlevel <= lenY){
 			curjobs++;
+//			printf("curlevel: %d, lenY: %d, curjobs: %d, thread: %d\n", curlevel, lenY, curjobs, thread);
+		}
 
-		startx = curlevel - 1;
-		starty = 0;
+		startx = poolsize + curlevel - 1;
+		starty = poolsize;
 
 		if (curlevel > tileX){
 			curjobs--;	
-			startx = tileX -1;
-			starty = curlevel - tileX;
+			startx = tableX -1;
+			starty = poolsize + curlevel - tileX;
+
+//			printf("curlevel: %d, curjobs: %d, thread: %d\n", curlevel,  curjobs, thread);
 		}
 	
 		if (thread < curjobs){
 			startx -= thread;
 			starty += thread;
-			startIdx = startx + starty * tileX;
-			tile[startIdx] = 
+			startIdx = startx + starty * tableX;
+			table[startIdx] = max(table[startIdx - 1], table[startIdx - tableX]);
+			x = startx - poolsize;
+			y = starty - poolsize;
+			if (dev_arr1[x] == dev_arr2[y])
+				table[startIdx] = table[startIdx - tableX - 1] + 1;				
+//			printf("curlevel: %d, curjobs: %d, thread: %d, idx: %d, startx: %d, starty: %d, startIdx: %d\n", curlevel, curjobs, thread, idx, startx, starty, startIdx);
 		}
 
 		curlevel++;
 		
+	
 		__syncthreads();	
+	}
+	
+	idx = thread;
+	address = thread;
+	while (idx < tilesize){
+		dev_table[address] = table[idx];
+		address += rowsize;
+		idx += blockDim.x;
 	}
 }
 
@@ -114,11 +115,8 @@ int LCS(int n1, int n2, int *arr1, int *arr2){
 
 	int maxthreads;
 	int maxlevel;
-	int curlevel = 1;
-	int curjobs = 1;
-	int startx, starty;
-	int threadPerBlock = 128, blockPerGrid;
-	int numStream = 16;
+	int threadPerBlock = tileY + poolsize, blockPerGrid = 1;
+	int numStream = 32;
 
 	cudaDeviceSetCacheConfig(cudaFuncCachePreferShared);
 
@@ -127,7 +125,7 @@ int LCS(int n1, int n2, int *arr1, int *arr2){
 	int maxSegThreads = min(xseg, yseg);		//max number of segs at either of the seg levels.
 	int maxSegLevel = xseg + yseg - 1;
 	int curSegLevel = 1;
-	int curSegJobs = 1;
+//	int curSegJobs = 1;
 	int startSegX, startSegY;	
 
 	cudaStream_t stream[numStream];
@@ -135,22 +133,22 @@ int LCS(int n1, int n2, int *arr1, int *arr2){
 		cudaStreamCreate(&stream[s]);
 
 	while(curSegLevel <= maxSegLevel){
-		int startSegIdx;
 		int segIdx = 0;
 		if (curSegLevel <= xseg){
-			startSegIdx = curSegLevel - 1;
-			curSegJobs = curSegLevel;
-			startSegX = startSegIdx;
+			//curSegJobs = curSegLevel;
+			startSegX = curSegLevel - 1;
 			startSegY = 0;
 		}	
 		else{
-			startSegIdx = xseg - 1 + xseg * (curSegLevel - xseg);
-			curSegJobs = 2 * segx - curSegLevel;
+			//startSegIdx = xseg - 1 + xseg * (curSegLevel - xseg);
+			//curSegJobs = 2 * xseg - curSegLevel;
 			startSegX = xseg - 1;
 			startSegY = curSegLevel - xseg;
 		}
 		
-		while ( startSegX > 0 && startSegY <= yseg - 1){
+		cout << "curSegLevel: " << curSegLevel << ", maxSegLevel: " << maxSegLevel << endl;	
+		
+		while ( startSegX >= 0 && startSegY <= yseg - 1){
 			//suppose n2 is the row size and the longer array
 			//int i = poolsize + startSegX * tileX;
 			//int j = poolsize + startSegY * tileY;
@@ -162,41 +160,23 @@ int LCS(int n1, int n2, int *arr1, int *arr2){
 			int resY = n1 - startSegY * tileY;
 			int lenY = min(resY, tileY);
 			maxlevel = tileX + lenY - 1;
-			maxThreads = min(tileX, lenY);
+			maxthreads = min(tileX, lenY);
 			int tilesize = (tileX+poolsize) * (lenY+poolsize);
 
-			GPU<<<1, threadPerBlock, 0, stream[s]>>>(tilesize, poolsize, threadPerBlock, maxThreads, &dev_table[startSegAdd], rowsize, tileX, lenY);
-
-			//This code has to ensure n2 size is the multiple of 128.
-			while (curlevel <= maxlevel){				
-				if (curlevel <= tileX){
-					startIdx = startSegAdd + curlevel - 1;
-					curjobs = min(curlevel, maxthreads);
-					startx = startIdx;
-					starty = 0;
-				}
-				else{
-					startx = tileX -1;
-					starty = curlevel - tileX;
-					startIdx = startSegAdd + tileX - 1 + rowsize * starty;
-					curjobs = tileX + lenY - curlevel;
-				}
-				int numthreads = (curjobs + 31) / 32;
-				numthreads *= 32;
-				blockPerGrid = (numthreads + threadPerBlock - 1) / threadPerBlock;
-
-				GPU<<<blockPerGrid, threadPerBlock, 0, stream[s]>>>(&dev_table[], dev_arr1, dev_arr2, startIdx, curjobs, rowsize, startx, starty);
-
-				cudaStreamSynchronize(stream[s]);
-				
-				curlevel++;
-			}
+			GPU<<<blockPerGrid, threadPerBlock, 0, stream[s]>>>(tilesize, poolsize, maxthreads, &dev_table[startSegAdd], rowsize, 
+										maxlevel, tileX, lenY, &dev_arr1[i], &dev_arr2[j]);
+			cudaStreamSynchronize(stream[s]);
+		
+//			cout << "startSegX: " << startSegX << ", startSegY: " << startSegY << ", segIdx: " << segIdx << endl;
 			startSegX--;
 			startSegY++;
 			segIdx++;
 		}
+		cout << endl;
 		//this synchronization is might removable
 		cudaDeviceSynchronize();
+
+		curSegLevel++;
 	}
 	
 	cudaMemcpy(&lcslength, &dev_table[tablesize-1], sizeof(int), cudaMemcpyDeviceToHost);
@@ -213,7 +193,7 @@ int LCS(int n1, int n2, int *arr1, int *arr2){
 */	
 
 	for (int s=0; s<numStream; s++)
-		cudaStreamDestroy[stream[s]];
+		cudaStreamDestroy(stream[s]);
 	
 	cudaFree(dev_arr1);
 	cudaFree(dev_arr2);
