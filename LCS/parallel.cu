14f058d536eba5b12c65b490df828f35ebfd05dd
Yuanzhe Li <zzzhe1990@gmail.com>
2018-04-30 15:17:04 -0400

    developing

--- LCS/parallel.cu
+++ LCS/parallel.cu
@@ -28,6 +28,52 @@ __global__ void GPU(int *dev_table, int *dev_arr1, int *dev_arr2, int startIdx, 
 //	__threadfence();
 }
 
+
+
+__global__ void GPU(int tilesize, const int threadNum, const int maxThreads, int *dev_table, const int rowsize, const int maxlevel){
+	//This code has to ensure n2 size is the multiple of 128. And n2 is no smaller than n1, where n2 is row array size, n1 is colum array size
+	//on K40, tile size is max to 48K, which is 128*96; on pascal and volta, tile size is max to 64K which is 128*128
+	__shared__ int tile[12288];
+
+	int thread = blockIdx.x * blockDim.x + threadIdx.x;
+	int idx = thread;
+	int address = thread;
+	int curjobs = 0;
+	int curlevel = 1;
+	int startIdx, startx, starty;
+
+	while(idx < tilesize){
+		tile[idx] = dev_table[address];
+		address += rowsize;
+		idx += threadNum;
+	}
+	
+	while (curlevel <= maxlevel){
+		if (curjobs < lenY)
+			curjobs++;
+
+		startx = curlevel - 1;
+		starty = 0;
+
+		if (curlevel > tileX){
+			curjobs--;	
+			startx = tileX -1;
+			starty = curlevel - tileX;
+		}
+	
+		if (thread < curjobs){
+			startx -= thread;
+			starty += thread;
+			startIdx = startx + starty * tileX;
+			tile[startIdx] = 
+		}
+
+		curlevel++;
+		
+		__syncthreads();	
+	}
+}
+
 void checkGPUError(cudaError err){
 	if (cudaSuccess != err){
 		printf("CUDA error in file %s, in line %i: %s\n", __FILE__, __LINE__, cudaGetErrorString(err));
@@ -38,6 +84,8 @@ void checkGPUError(cudaError err){
 int LCS(int n1, int n2, int *arr1, int *arr2){
 	int lcslength;
 	int poolsize = 32;
+	int tileX = 128;
+	int tileY = 96;
 	int rowsize = poolsize + n2;
 	int colsize = poolsize + n1;
 
@@ -63,42 +111,91 @@ int LCS(int n1, int n2, int *arr1, int *arr2){
 	cudaMemcpy(dev_arr1, arr1, n1*sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_arr2, arr2, n2*sizeof(int), cudaMemcpyHostToDevice);
 
-	int maxthreads = min(n1, n2);
-	int maxlevel = n1 + n2 - 1;	
+	int maxthreads;
+	int maxlevel;
 	int curlevel = 1;
 	int curjobs = 1;
 	int startx, starty;
-	int threadPerBlock = 1024, blockPerGrid;
+	int threadPerBlock = 128, blockPerGrid;
+	int numStream = 16;
 
-	//suppose n2 is the row size and the longer array
-	while(curlevel <= maxlevel){
-//		cout << "level: " << curlevel << endl;
-		int startIdx;
-		if (curlevel <= n2){
-			startIdx = curlevel - 1;
-			curjobs = curlevel;
-			startx = startIdx;
-			starty = 0;
-		}
+	cudaDeviceSetCacheConfig(cudaFuncCachePreferShared);
+
+	int xseg = (n1+tileX-1) / tileX;
+	int yseg = (n2+tileY-1) / tileY;
+	int maxSegThreads = min(xseg, yseg);		//max number of segs at either of the seg levels.
+	int maxSegLevel = xseg + yseg - 1;
+	int curSegLevel = 1;
+	int curSegJobs = 1;
+	int startSegX, startSegY;	
+
+	cudaStream_t stream[numStream];
+	for (int s=0; s<numStream; s++)
+		cudaStreamCreate(&stream[s]);
+
+	while(curSegLevel <= maxSegLevel){
+		int startSegIdx;
+		int segIdx = 0;
+		if (curSegLevel <= xseg){
+			startSegIdx = curSegLevel - 1;
+			curSegJobs = curSegLevel;
+			startSegX = startSegIdx;
+			startSegY = 0;
+		}	
 		else{
-			startIdx = n2 - 1 + rowsize * (curlevel - n2);
-			curjobs = 2 * n2 - curlevel;
-			startx = n2 - 1;
-			starty = curlevel - n2;
+			startSegIdx = xseg - 1 + xseg * (curSegLevel - xseg);
+			curSegJobs = 2 * segx - curSegLevel;
+			startSegX = xseg - 1;
+			startSegY = curSegLevel - xseg;
 		}
-
-		int numthreads = (curjobs + 31) / 32;
-		numthreads *= 32;
-	
-		blockPerGrid = (numthreads + threadPerBlock - 1) / threadPerBlock;
-
-		GPU<<<blockPerGrid, threadPerBlock>>>(&dev_table[poolsize*rowsize+poolsize], dev_arr1, dev_arr2, startIdx, curjobs, rowsize, startx, starty);		
 		
-		cudaDeviceSynchronize();		
+		while ( startSegX > 0 && startSegY <= yseg - 1){
+			//suppose n2 is the row size and the longer array
+			int i = poolsize + startSegX * tileX;
+			int j = poolsize + startSegY * tileY;
+			int startSegAdd = j * (n2 + poolsize) + i;
+			int s = segIdx % numStream;
+			//resY is used to determine the rest size of Y. This is used to check if the rest size of Y is smaller than tileY.
+			int resY = n1 - startSegY * tileY;
+			int lenY = min(resY, tileY);
+			maxlevel = tileX + lenY - 1;
+			maxThreads = min(tileX, lenY);
+			int tilesize = tileX * lenY;
 
-		curlevel++;
+			GPU<<<1, threadPerBlock, 0, stream[s]>>>(tilesize, threadPerBlock, maxThreads, &dev_table[startSegAdd], rowsize);
+
+			//This code has to ensure n2 size is the multiple of 128.
+			while (curlevel <= maxlevel){				
+				if (curlevel <= tileX){
+					startIdx = startSegAdd + curlevel - 1;
+					curjobs = min(curlevel, maxthreads);
+					startx = startIdx;
+					starty = 0;
+				}
+				else{
+					startx = tileX -1;
+					starty = curlevel - tileX;
+					startIdx = startSegAdd + tileX - 1 + rowsize * starty;
+					curjobs = tileX + lenY - curlevel;
+				}
+				int numthreads = (curjobs + 31) / 32;
+				numthreads *= 32;
+				blockPerGrid = (numthreads + threadPerBlock - 1) / threadPerBlock;
+
+				GPU<<<blockPerGrid, threadPerBlock, 0, stream[s]>>>(&dev_table[], dev_arr1, dev_arr2, startIdx, curjobs, rowsize, startx, starty);
+
+				cudaStreamSynchronize(stream[s]);
+				
+				curlevel++;
+			}
+			startSegX--;
+			startSegY++;
+			segIdx++;
+		}
+		//this synchronization is might removable
+		cudaDeviceSynchronize();
 	}
-
+	
 	cudaMemcpy(&lcslength, &dev_table[tablesize-1], sizeof(int), cudaMemcpyDeviceToHost);
 	//cudaMemcpy(table, dev_table, (n1+poolsize)*rowsize*sizeof(int), cudaMemcpyDeviceToHost);
 /*
@@ -112,6 +209,9 @@ int LCS(int n1, int n2, int *arr1, int *arr2){
 	}
 */	
 
+	for (int s=0; s<numStream; s++)
+		cudaStreamDestroy[stream[s]];
+	
 	cudaFree(dev_arr1);
 	cudaFree(dev_arr2);
 	cudaFree(dev_table);
