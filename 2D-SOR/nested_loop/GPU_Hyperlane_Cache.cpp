44f45a55c17f926aaf7fd513f2bfcbeb409ae09f
Yuanzhe Li <zzzhe1990@gmail.com>
2018-12-31 14:35:43 -0500

    2D-SOR nested loop updated

--- 2D-SOR/nested_loop/GPU_Hyperlane_Cache.cpp
+++ 2D-SOR/nested_loop/GPU_Hyperlane_Cache.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 //#define DEBUG
 #define batchexe
+const int itr=100;
 
 void readInputData(string str1, int &n1, int &n2, int **arr){
 	ifstream inputfile;
@@ -82,15 +83,15 @@ int main(int argc, char **argv){
 
 	gettimeofday(&tbegin, NULL);
 #ifdef batchexe
-	for (int i=0; i<100; i++)
+	for (int i=0; i<itr; i++)
 #endif	
 	SOR(n1, n2, arr);
 
 	gettimeofday(&tend, NULL);
 
-	double s = (double)(tend.tv_sec - tbegin.tv_sec) + (double)(tend.tv_usec - tbegin.tv_usec)/1000000.0;
+	double s = (double)(tend.tv_sec - tbegin.tv_sec)*1000.0 + (double)(tend.tv_usec - tbegin.tv_usec)/1000.0;
 
-	cout << "execution time: " << s << " second." << endl;
+	cout << "execution time: " << s/itr << " second." << endl;
 #ifdef DEBUG
 	string outfile = "./Output/output_LCS_";
 	outfile.append(convert1.str());
