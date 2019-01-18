e62460c2f24ce7d02a206d2f8a6fb0310c8981a7
zzzhe1990 <zzzhe1990@gmail.com>
2019-01-18 14:33:06 -0800

    script updated

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-METRICS=gld_efficiency,gst_efficiency,shared_efficiency,tex_cache_hit_rate,l2_tex_read_hit_rate
-SPATH=~/Desktop/GPUMemEfficient/SmithWaterman/
+METRICS="gld_efficiency,gst_efficiency,shared_efficiency,tex_cache_hit_rate,l2_tex_read_hit_rate"
+SPATH='~/Desktop/GPUMemEfficient/SmithWaterman/'
 cd SPATH
 nvprof --metrics METRICS --log-file share_nvprof_1080ti_128_32.log ./GPU_Hyperlane_Share 15 15 32 128 1 
 nvprof --metrics METRICS --log-file share_nvprof_1080ti_256_32.log ./GPU_Hyperlane_Share 15 15 32 256 1
